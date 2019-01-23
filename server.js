b76fc1b87b8837af9fa4277d7abbee715af6c658
Connor Oswold <coswoldo@gmail.com>
2019-01-23 15:24:29 -0800

    install express and handlebars

--- server.js
+++ server.js
@@ -11,4 +11,19 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
+// NEW
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new')
+})
+
+// CREATE
+app.post('/posts', (req, res) => {
+    Post.create(req.body).then((post) => {
+        console.log(post);
+        res.redirect(`/post/${review._id}`)
+    }).catch((err) => {
+        console.log(err.message)
+    })
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
