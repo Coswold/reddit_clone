12ab7e512b9969ab3848bea9da796a8af31e20cd
Connor Oswold <coswoldo@gmail.com>
2019-01-29 14:19:47 -0800

    show single post

--- controllers/posts.js
+++ controllers/posts.js
@@ -28,4 +28,16 @@ module.exports = function(app) {
             return res.redirect(`/`);
         })
     });
+
+    // SHOW
+    app.get("/posts/:id", function(req, res) {
+        // LOOK UP THE POST
+        Post.findById(req.params.id)
+        .then(post => {
+            res.render("posts-show", { post });
+        })
+        .catch(err => {
+            console.log(err.message);
+        });
+    });
 }
